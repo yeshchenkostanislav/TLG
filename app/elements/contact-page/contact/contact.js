2a6c4e396c8e084952e296f9f0503ded10f77233
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-10-19 15:42:51 +0200

    внесение изменений

--- app/elements/contact-page/contact/contact.js
+++ app/elements/contact-page/contact/contact.js
@@ -1,12 +1,31 @@
 $('.contact__btn').click(function () {
 
   $('.contact__input').each(function (i, elem) {
-    //alert($(this).val())
-
-    if ($(elem).val().length() > 0) {
-      alert($(elem).val())
-    }
+    if (!$(this).val()) {
+      $(this).addClass('contact__input_empty');
+    } else {
+      $(this).removeClass('contact__input_empty');
+    };
   });
+
+  if ($(".contact__input").is(".contact__input_empty")) {
+    //alert('Прошу заполнить поля');
+  } else {
+    SendPost();
+    $('.contact__input').val("");
+  }
+
+  /* Ajax-запрос */
+  function SendPost() {
+    jQuery.ajax({
+      type: "GET",
+      url: "../19/send.php",
+      data: jQuery("#form").serialize(),
+      success: function () {
+        alert('работает');
+      }
+    });
+  }
 });
 
 $('.contact__input').blur(function () //whenever you click off an input element
