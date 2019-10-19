39ed677d2006d2c7854fb8f03746b1fb623edf9a
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-10-19 13:29:52 +0200

    правки

--- app/elements/common-elements/header/header.js
+++ app/elements/common-elements/header/header.js
@@ -1,6 +1,20 @@
-$('.header__menu-btn').click(function () {
+$(document).mouseup(function (e) { // событие клика по веб-документу
+  var div = $(".header__menu"); // тут указываем ID элемента
+  if (!div.is(e.target) // если клик был не по нашему блоку
+    &&
+    div.has(e.target).length === 0) { // и не по его дочерним элементам
+    $('.header__nav').removeClass('header__nav_active');
+    $('.header__menu-btn').removeClass('header__menu-btn_active');
+  }
+})
+$('.header__menu-btn-wrap').click(function () {
 
   $('.header__nav').toggleClass('header__nav_active');
-  $(this).toggleClass('header__menu-btn_active');
+  $('.header__menu-btn').toggleClass('header__menu-btn_active');
 
+});
+
+$('.header__nav-item').on('click', function () {
+  $('.header__nav-item').removeClass('header__nav-item_active');
+  $(this).addClass('header__nav-item_active');
 })
